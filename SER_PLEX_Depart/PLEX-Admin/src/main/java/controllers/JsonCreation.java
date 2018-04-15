844f8a7ac49583424c10920eddb07ce6ef26b052
zutt <yannlederrey@gmail.com>
2018-04-15 20:09:01 +0200

    creation JSon

--- SER_PLEX_Depart/PLEX-Admin/src/main/java/controllers/JsonCreation.java
+++ SER_PLEX_Depart/PLEX-Admin/src/main/java/controllers/JsonCreation.java
@@ -2,14 +2,10 @@ package controllers;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import models.Acteur;
-import models.GlobalData;
-import models.Projection;
+import models.*;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class JsonCreation {
     private GlobalData globalData;
@@ -20,19 +16,43 @@ public class JsonCreation {
 
     public void create() {
 
-        //globalData.getProjections();
-
-        List<Projection> Projections = new ArrayList<>();
+        ProjectionsJson projectionsJson = new ProjectionsJson();
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         for (Projection projection : globalData.getProjections()) {
-            Projections.add(projection);
+
+            // Récupére le 1er ROle et le deuxième
+            int i = 0;
+            RoleActeur role1 = new RoleActeur();
+            RoleActeur role2  = new RoleActeur();
+
+            for(RoleActeur role : projection.getFilm().getRoles()){
+                if(role.getPlace() == 1){
+                    role1 = role;
+                    i++;
+                }
+                if(role.getPlace() == 2) {
+                    role2 = role;
+                    i++;
+                }
+                if(i >= 2)
+                    break;
+            }
+
+            // créé les acteurs
+            ActeurJson acteurs = new ActeurJson(role1.getActeur().getNom(), role2.getActeur().getNom());
+
+            // créé la projection
+            ProjectionJson Projections = new ProjectionJson(projection.getFilm().getTitre(),
+                    projection.getDateHeure().getTime().toString(), acteurs);
+
+            projectionsJson.addProjection(Projections);
         }
 
         try (FileWriter writer = new FileWriter("json.json")) {
 
-            gson.toJson(Projections, writer);
+            gson.toJson(projectionsJson, writer);
 
         } catch (IOException e) {
             e.printStackTrace();
